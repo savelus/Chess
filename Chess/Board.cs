7d900fba6d400a0b781f38afca6ecef016bd90f0
Савелий <savelussava@mail.ru>
2021-06-23 11:04:14 +0500

    Add Board

---
+++ Chess/Board.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess
+{
+    public class Cell
+    {
+        public bool IsEmpty { get; set; } = true;
+        public IFigure FigureInCell { get; set; }
+    }
+    public class Board
+    {
+        public Cell[,] Field = new Cell[8, 8];
+        public Board()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Field[i, j] = new Cell();
+                }
+            }
+        }
+    }
+}
